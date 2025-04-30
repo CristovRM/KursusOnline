7af060474649f28d5f0110940910f1bb5c634e02
Cristov Relevando Manurung <Cristovrm2@gmail.com>
2025-05-01 05:25:20 +0700

    buat halaman about,contact,course,home,login, styling inheritence yang diambil dari homebase dan membuat mereka di urls.py dan views.py

--- KursusOnline/main/urls.py
+++ KursusOnline/main/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("login", views.login, name="login"),
+    path("course", views.course, name="course"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
 ]
--- KursusOnline/main/views.py
+++ KursusOnline/main/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def home(request):
     context={}
     return render(request, "main/home.html", context)
+def login(request):
+    context={}
+    return render(request, "main/login.html", context)
+def course(request):
+    context={}
+    return render(request, "main/course.html", context)
+def about(request):
+    context={}
+    return render(request, "main/about.html", context)
+def contact(request):
+    context={}
+    return render(request, "main/contact.html", context)
