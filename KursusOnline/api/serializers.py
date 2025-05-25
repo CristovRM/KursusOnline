b41b7dd81e548ee2ec6bf9d0c1e6463e6ff09d55
Hendri Saputra <hendrisapputraa@gmail.com>
2025-05-25 22:10:38 +0700

    merubah password member di rest api menjadi type hash

--- KursusOnline/api/serializers.py
+++ KursusOnline/api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from main.models import Member  # ⬅️ ambil dari main
+from django.contrib.auth.hashers import make_password
 
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
-        fields = '__all__'
+        fields = ['id', 'nama', 'email', 'pekerjaan', 'password', 'role']
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            validated_data['password'] = make_password(validated_data['password'])
+        return super().update(instance, validated_data)
