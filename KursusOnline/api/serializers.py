a3fe74b4db3a25514cd27f566bfb346a2ea8f37b
Cristov Relevando Manurung <Cristovrm2@gmail.com>
2025-05-26 19:37:32 +0700

    melengkapi API

--- KursusOnline/api/serializers.py
+++ KursusOnline/api/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
-from main.models import Member  # ⬅️ ambil dari main
 from django.contrib.auth.hashers import make_password
+from main.models import (
+    Member, Kategori, Kursus, Transaksi, MateriKursus,
+    Rating, PendapatanPengajar, PendapatanAdmin, TugasAkhir, Sertifikat
+)
 
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +18,48 @@ class MemberSerializer(serializers.ModelSerializer):
         if 'password' in validated_data:
             validated_data['password'] = make_password(validated_data['password'])
         return super().update(instance, validated_data)
+
+class KategoriSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Kategori
+        fields = '__all__'
+
+class KursusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Kursus
+        fields = '__all__'
+
+class TransaksiSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaksi
+        fields = '__all__'
+
+class MateriKursusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MateriKursus
+        fields = '__all__'
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = '__all__'
+
+class PendapatanPengajarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PendapatanPengajar
+        fields = '__all__'
+
+class PendapatanAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PendapatanAdmin
+        fields = '__all__'
+
+class TugasAkhirSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TugasAkhir
+        fields = '__all__'
+
+class SertifikatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sertifikat
+        fields = '__all__'
--- KursusOnline/api/views.py
+++ KursusOnline/api/views.py
@@ -1,8 +1,50 @@
 from rest_framework import viewsets
-from main.models import Member  # ⬅️ ambil dari main
-from .serializers import MemberSerializer
+from main.models import (
+    Member, Kategori, Kursus, Transaksi, MateriKursus,
+    Rating, PendapatanPengajar, PendapatanAdmin, TugasAkhir, Sertifikat
+)
+from .serializers import (
+    MemberSerializer, KategoriSerializer, KursusSerializer, TransaksiSerializer,
+    MateriKursusSerializer, RatingSerializer, PendapatanPengajarSerializer,
+    PendapatanAdminSerializer, TugasAkhirSerializer, SertifikatSerializer
+)
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
+class KategoriViewSet(viewsets.ModelViewSet):
+    queryset = Kategori.objects.all()
+    serializer_class = KategoriSerializer
+
+class KursusViewSet(viewsets.ModelViewSet):
+    queryset = Kursus.objects.all()
+    serializer_class = KursusSerializer
+
+class TransaksiViewSet(viewsets.ModelViewSet):
+    queryset = Transaksi.objects.all()
+    serializer_class = TransaksiSerializer
+
+class MateriKursusViewSet(viewsets.ModelViewSet):
+    queryset = MateriKursus.objects.all()
+    serializer_class = MateriKursusSerializer
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+
+class PendapatanPengajarViewSet(viewsets.ModelViewSet):
+    queryset = PendapatanPengajar.objects.all()
+    serializer_class = PendapatanPengajarSerializer
+
+class PendapatanAdminViewSet(viewsets.ModelViewSet):
+    queryset = PendapatanAdmin.objects.all()
+    serializer_class = PendapatanAdminSerializer
+
+class TugasAkhirViewSet(viewsets.ModelViewSet):
+    queryset = TugasAkhir.objects.all()
+    serializer_class = TugasAkhirSerializer
+
+class SertifikatViewSet(viewsets.ModelViewSet):
+    queryset = Sertifikat.objects.all()
+    serializer_class = SertifikatSerializer
